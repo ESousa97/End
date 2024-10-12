85e03fe38d616229e87510e7049d391dad7f1122
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-10-12 20:39:32 -0300

    att finish project.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 let currentIndex = 0;
-
 const images = document.querySelectorAll('.carousel-images img');
 const totalImages = images.length;
+const lightbox = document.getElementById('lightbox');
+const lightboxImage = document.getElementById('lightbox-image');
 
+// Função para mostrar a próxima imagem
 document.querySelector('.next-btn').addEventListener('click', showNextImage);
 document.querySelector('.prev-btn').addEventListener('click', showPrevImage);
 
@@ -17,3 +19,16 @@ function showPrevImage() {
     const carouselImages = document.querySelector('.carousel-images');
     carouselImages.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
+
+// Adiciona o evento de clique nas imagens para ativar o zoom
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        lightboxImage.src = img.src; // Define a imagem selecionada no lightbox
+        lightbox.style.display = 'flex'; // Exibe o lightbox
+    });
+});
+
+// Fecha o lightbox ao clicar na imagem ampliada
+lightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none'; // Oculta o lightbox
+});
